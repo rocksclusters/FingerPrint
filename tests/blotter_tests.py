4b8011f07f6d12c177d9490065ab43003d2ef126
clem <luca.clementi@gmail.com>
2012-08-01 17:05:49 -0700

    Reordered unit tests
    All the test are now in one modules, cleaner output
    
    Signed-off-by: clem <luca.clementi@gmail.com>

--- tests/blotter_tests.py
+++ tests/blotter_tests.py
@@ -3,9 +3,14 @@
 import unittest
 import subprocess
 import os
+from datetime import datetime
 
+
+from FingerPrint.plugins import PluginManager
 from FingerPrint.blotter import Blotter
 from FingerPrint.swirl import Swirl
+import FingerPrint.sergeant
+
 
 class TestSequenceFunctions(unittest.TestCase):
 
@@ -13,32 +18,60 @@ class TestSequenceFunctions(unittest.TestCase):
         """setup for your unittest"""
         import sys
         sys.path.append("../")
+        #TODO find a better way to find filelist
         self.files = ["/bin/ls", "/usr/bin/find", "/etc/passwd", 
             "/lib/libcryptsetup.so.4.0.0", "/lib/x86_64-linux-gnu/libm.so.6",
             "/lib/x86_64-linux-gnu/libm-2.15.so"]
+        self.swirlfile = "tests/test.swirl"
+        self.availablePlugin = 2
+
+
+    def test_plugin(self):
+        print "     -----------------------     Testing pluging manager via API  -------------------------"
+        self.swirl = Swirl("test", datetime.now())
+        for i in self.files:
+            swirlFile = PluginManager.getSwirl(i)
+            self.swirl.addFile(swirlFile)
+        p = PluginManager.get_plugins()
+        print "plugins: ", p
+        self.assertEqual(len(p), self.availablePlugin,
+            msg="Plugin manager did not load all the available plugins (available %d, detected %d) " 
+            % (len(p) , self.availablePlugin) )
+            
+
+
+    def test_sergeant(self):
+        print "     -----------------------     Verifying sergeant via API     -------------------------"
+        # test loading file in blotter
+        print "file %s loaded" % self.swirlfile
+        serg = FingerPrint.sergeant.readFromPickle(self.swirlfile)
+        self.assertTrue( serg.check(), msg="API: creating sergeant from %s failed." 
+                % self.swirlfile)
 
     def test_blotter(self):
         # test loading file in blotter
-        print "     -----------------------      creating a blotter via API     -------------------------"
+        print "     -----------------------     Creating a blotter via API     -------------------------"
         b=Blotter("Test", self.files)
-        print "swirl structure:\n", b.getSwirl()
-        #self.assertEqual(self.seq, range(10))
-        print "list of global dependecies:\n", b.getSwirl().getDependencies()
-        print "list of global provides:\n", b.getSwirl().getProvides()
+        self.assertIsNotNone(b.getSwirl())
+        self.assertTrue( len(b.getSwirl().getDependencies()) > 0, 
+                msg="API: blotter could not find any dependency")
+        self.assertTrue( len(b.getSwirl().getProvides()) > 0, 
+                msg="API: blotter could not find any provides")
+
 
     def test_commandline(self):
         """ """
         #test empty command line
-        print "     -----------------------  running fingerprint command line   -------------------------"
+        print "     -----------------------     Running fingerprint command line   -------------------------"
         self.assertNotEqual(subprocess.call(['python', './scripts/fingerprint']), 0,
-            msg="empty command line should fail but it did not")
-        #lets create a command wtih input file on the command line
+            msg="fingerprint: empty command line should fail but it did not")
+        #lets create a command wtih input file on the command line with default output filename
         outputfilename='output.swirl'
         self.assertEqual( 
-            subprocess.call(['python', './scripts/fingerprint', '-f', outputfilename] + self.files), 0,
-                msg="fingerprint failed to analize the files: " + str(self.files))
+            subprocess.call(['python', './scripts/fingerprint', '-c'] + self.files), 0,
+            msg="fingerprint-create: failed to analize the files: " + str(self.files))
         self.assertTrue( os.path.isfile(outputfilename), 
-            msg="the output file %s was not created properly" % outputfilename )
+            msg="fingerprint-create: the output file %s was not created properly" % outputfilename )
         os.remove(outputfilename)
         filelist='filelist'
         fd=open(filelist,'w')
@@ -46,10 +79,10 @@ class TestSequenceFunctions(unittest.TestCase):
             fd.write(i + '\n')
         fd.close()
         self.assertEqual( 
-            subprocess.call(['python', './scripts/fingerprint', '-f', outputfilename, '-l', filelist]), 0,
-            msg="fingerprint failed to load filelist: " + filelist)
+            subprocess.call(['python', './scripts/fingerprint', '-c', '-f', outputfilename, '-l', filelist]), 0,
+            msg="fingerprint-create: failed to load filelist: " + filelist)
         self.assertTrue( os.path.isfile(outputfilename), 
-            msg="the output file %s was not created properly" % outputfilename )
+            msg="fingerprint-create: the output file %s was not created properly" % outputfilename )
         os.remove(outputfilename)
 
 
