ed62dc707efa2ac2c0c67c17d4253414aedd563a
clem <luca.clementi@gmail.com>
2012-08-07 12:46:56 -0700

    First version of the new python pluging getSwirl function
    Signed-off-by: clem <luca.clementi@gmail.com>

--- FingerPrint/plugins/python.py
+++ FingerPrint/plugins/python.py
@@ -5,7 +5,9 @@
 # base class for the fingerprint plugin classes
 #
 
-import os
+import sys, os
+import parser, symbol, types
+
 
 from FingerPrint.swirl import SwirlFile, Dependency
 from FingerPrint.plugins import PluginManager
@@ -19,9 +21,12 @@ class PythonPlugin(PluginManager):
     """this plugin manages only python source code file for now"""
 
     pluginName="python"
+    #TODO find this dinamically
+    _prefix = "python%u.%u" % sys.version_info[0:2]
+    _buildin_modules = sys.builtin_module_names
  
     @classmethod
-    def isDepsatisfied(self, dependency):
+    def isDepsatisfied(cls, dependency):
         """verify that the dependency passed can be satified on this system
         and return True if so
         """
@@ -29,49 +34,56 @@ class PythonPlugin(PluginManager):
         
         return True
 
-        
 
+
+    # Preliminary hack to python 2.4 compatibility
     @classmethod
-    def setDepsRequs(self, fileName):
-        """given a fileName pointing to a python script it returns a swirlFile 
-        with all the dependency and provide associated with the fileName
-        """
-        swirlFile = SwirlFile( fileName )
-        swirlFile.setPluginName( self.pluginName )
-        swirlFile.dyn = True
-        #find deps
-        fd=open(fileName)
-        #this is as quick and as dirty as it could be
-        for line in fd:
-            if 'import ' in line and not '"' in line:
-                #this is an import line
-                #TODO this is not sufficient but it will get me started
-                newDep = Dependency( line.strip() )
-                newDep.setPluginName( self.pluginName )
-                swirlFile.addDependency( newDep )
-        #TODO add provide
-        return swirlFile
-
+    def _pro(cls, x) :
+        return x[1]
                 
+    @classmethod
+    def _match(cls, tree) :
+        """generator function it return an iterator over a list of 
+        string which represents the dependency"""
+        for node in tree :
+            if type(node) in [types.ListType, types.TupleType] :
+                if node[0] == symbol.import_stmt :
+                    node = _pro(node)
+                    if node[1][1] == 'import' :
+                        for name in [x for x in node[2][1:] if x[0] != 12  ] :
+                            yield ".".join( [ i for t,i in  name[1][1:] if t==1 ])
+                    elif node[1][1] == 'from' :
+                        yield ".".join( [ i for t,i in  node[2][1:] if t==1 ])
+    
+                for item in match(node) :
+                    yield item
+
 
     @classmethod
-    def getSwirl(self, fileName):
-        """helper function given a filename it return a Swirl 
-        if the given plugin does not support the given fileName should just 
-        return None
-        ATT: only one plugin should return a SwirlFile for a given file
-        """
-        #python script
-        if fileName.endswith('.py'):
-            return self.setDepsRequs(fileName)
-        #let's check more accurately
-        fd=open(fileName)
-        for line in fd:
-            if line.startswith('#!') and 'python' in line:
-                #ok this is a python script
-                fd.close()
-                return self.setDepsRequs(fileName)
-        fd.close()
-        return None
+    def getSwirl(cls, fileName) :
+        """Given a path to a python file it return a list of dependecy"""
+        returnList = []
+        try :
+            lis = parser.suite(file(fileName).read().rstrip().replace("\r\n","\n")).tolist()
+        except SyntaxError,msg :
+            #print "Not a python file"
+            return None
+        except :
+            return None
+        else :
+            swirlFile = SwirlFile( fileName )
+            swirlFile.setPluginName( self.pluginName )
+            swirlFile.dyn = True
+            for item in cls._match(lis) :
+                #it is a python file
+                if item not in _buildin_modules :
+                    newDep = Dependency( "%s(%s)" % (_prefix,item) )
+                    newDep.setPluginName( self.pluginName )
+                    swirlFile.addDependency( newDep )
+        return swirlFile
+    
+    
+
+
 
        
