15f9b7197a3a1ddd40d6c865912bfd06a1fc80f1
clem <luca.clementi@gmail.com>
2012-08-07 17:55:10 -0700

    Directories are silently ignored during swirl creation
     - Directories are silently ignored during swirl creation
     - Nicer error reporting
    
    Signed-off-by: clem <luca.clementi@gmail.com>

--- FingerPrint/blotter.py
+++ FingerPrint/blotter.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from swirl import Swirl
 from FingerPrint.plugins import PluginManager
 
-
+import os
 
 
 """The getDependencies functions given a swirl file the have to figure out 
@@ -23,8 +23,14 @@ class Blotter:
         """give a file list and a name construct a swirl into memory """
         self.swirl = Swirl(name, datetime.now())
         for i in fileList:
-            swirlFile = PluginManager.getSwirl(i)
-            self.swirl.addFile(swirlFile)
+            if os.path.isfile(i):
+                swirlFile = PluginManager.getSwirl(i)
+                self.swirl.addFile(swirlFile)
+            elif os.path.isdir(i):
+                pass
+            else:
+                raise IOError("The file %s cannot be opened." % i)
+                
 
        
     def getSwirl(self):
