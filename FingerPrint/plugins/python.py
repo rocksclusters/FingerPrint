f22397c4c3d5e0fd4e2d01fcf5045656fda82c42
clem <luca.clementi@gmail.com>
2012-08-16 16:21:43 -0700

    Added file detection capability to the python plugin
    Code is ugly and needs some cleanup
    
    Signed-off-by: clem <luca.clementi@gmail.com>

--- FingerPrint/plugins/python.py
+++ FingerPrint/plugins/python.py
@@ -7,6 +7,7 @@
 
 import sys, os
 import parser, symbol, types
+import re
 
 
 from FingerPrint.swirl import SwirlFile, Dependency
@@ -25,7 +26,6 @@ class PythonPlugin(PluginManager):
     """this plugin manages only python source code file for now"""
 
     pluginName="python"
-    #TODO find this dinamically
     _prefix = "python%u.%u" % sys.version_info[0:2]
     _buildin_modules = sys.builtin_module_names
  
@@ -38,23 +38,6 @@ class PythonPlugin(PluginManager):
         return True
 
                 
-    @classmethod
-    def _match(cls, tree) :
-        """generator function it return an iterator over a list of 
-        string which represents the dependency"""
-        for node in tree :
-            if type(node) in [types.ListType, types.TupleType] :
-                if node[0] == symbol.import_stmt :
-                    node = _pro(node)
-                    if node[1][1] == 'import' :
-                        for name in [x for x in node[2][1:] if x[0] != 12  ] :
-                            yield ".".join( [ i for t,i in  name[1][1:] if t==1 ])
-                    elif node[1][1] == 'from' :
-                        yield ".".join( [ i for t,i in  node[2][1:] if t==1 ])
-    
-                for item in cls._match(node) :
-                    yield item
-
 
     @classmethod
     def getSwirl(cls, fileName) :
@@ -64,7 +47,6 @@ class PythonPlugin(PluginManager):
             f = file(fileName)
             data = f.read().rstrip().replace("\r\n","\n")
             lis = parser.suite(data).tolist()
-            f.close()
         except SyntaxError,msg :
             #print "Not a python file"
             return None
@@ -75,18 +57,75 @@ class PythonPlugin(PluginManager):
             swirlFile.setPluginName( cls.pluginName )
             swirlFile.type = cls.pluginName
             swirlFile.dyn = True
-            for item in cls._match(lis) :
-                #it is a python file
-                newdepName = cls._prefix + "(" + item + ")"
+            #
+            # let's check which interpreter should we use
+            #
+            f.seek(0)
+            header = f.readline().rstrip()
+            if header[0:2] == '#!':
+                pythonVer = re.split(' |/|!', header)[-1]
+            else:
+                pythonVer = cls._prefix
+            for item in cls._getModules(lis) :
+                newdepName = pythonVer + "(" + item + ")"
+                # newdepName is not in buildin_modules and
+                # it's really a new dep
                 if item not in cls._buildin_modules and \
                     newdepName not in [ i.depname for i in swirlFile.dependencies ]:
                     newDep = Dependency( newdepName )
                     newDep.setPluginName( cls.pluginName )
+                    #
+                    # we have to find which file provides this dep
+                    #
+                    #TODO move to a function
+                    paths = []
+                    #TODO load from a differen interpreter
+                    if pythonVer == 'python' or pythonVer == cls._prefix:
+                        #we can use this interpreter
+                        try:
+                            if '.' in item:
+                                # When the name variable is of the form package.module,
+                                # normally, the top-level package (the name up till the
+                                # first dot) is returned, not the module named by name.
+                                module = __import__(item, globals(),
+                                    locals(), item.split('.')[-1])
+                            else:
+                                module = __import__(item)
+                        except:
+                            print "Unable to import module %s, skipping..." % \
+                                (item)
+                            continue
+                        else:
+                            paths = [ os.path.abspath(module.__file__) ]
+                            if paths[0].endswith('.pyc'):
+                                temp = paths[0]
+                                paths = [temp[0:-1], temp]
+                    newDep.pathList = paths
                     swirlFile.addDependency( newDep )
+        finally:
+            f.close()
         return swirlFile
     
-    
 
 
+    @classmethod
+    def _getModules(cls, tree) :
+        """generator function it return an iterator over a list of
+        string which represents the dependency"""
+        for node in tree :
+            if type(node) in [types.ListType, types.TupleType] :
+                if node[0] == symbol.import_stmt :
+                    node = _pro(node)
+                    if node[1][1] == 'import' :
+                        for name in [x for x in node[2][1:] if x[0] != 12  ] :
+                            yield ".".join( [ i for t,i in  name[1][1:] if t==1 ])
+                    elif node[1][1] == 'from' :
+                        yield ".".join( [ i for t,i in  node[2][1:] if t==1 ] )
+                for item in cls._getModules(node) :
+                    yield item
 
-       
+    @classmethod
+    def _findFile(cls, newDep ):
+        pass
+
+
