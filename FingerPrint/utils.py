1b0ee88745e1619ee66f6281cf4d6b4e3ac02f7b
clem <luca.clementi@gmail.com>
2012-08-16 16:23:13 -0700

    Added which function.
    It will be used by the python pulgins
    
    Signed-off-by: clem <luca.clementi@gmail.com>

--- FingerPrint/utils.py
+++ FingerPrint/utils.py
@@ -10,3 +10,20 @@ def getOutputAsList(binary, inputString=None):
     grep_stdout = p.communicate(input=inputString)[0]
     return grep_stdout.split('\n')
 
+
+def which(program, extraPath):
+
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep) + extraPath:
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    return None
+
