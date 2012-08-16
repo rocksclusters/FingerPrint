dfb4f4f704fce2ffe60bdf411b67eaa23e87f512
clem <luca.clementi@gmail.com>
2012-08-15 17:25:20 -0700

    crapy script to run fingerprint in cronjob on a set of applications
    Signed-off-by: clem <luca.clementi@gmail.com>

---
+++ docs/runFingerprint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# LC
+#
+# script to run fingerprint on a set of application
+
+apps="amber"
+email="clem@sdsc.edu"
+
+
+
+. /etc/profile
+cd $HOME
+
+
+function failure(){
+    echo -n `date` - ERROR >> tmplog
+    if [ "$1" == 'y' ];then 
+        echo -n verification >> tmplog
+    elif [ "$1" == "i" ];then
+        echo -n integrity >> tmplog 
+    fi
+    cat error >> tmplog
+    cat tmplog | mail -s "error `hostname`" $email
+    cat tmplog >> log
+    rm tmplog
+    rm error
+}
+
+
+for i in $apps; do
+    cd $i
+    fingerprint -y > error || failure y $i
+    fingerprint -i > error || failure i $i
+    cd ..
+done
+
+
